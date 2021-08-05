448d20fb4f5814603877bfeccd3e35b1e7033c98
J-Nic <semato40@naver.com>
2021-08-05 20:31:56 +0900

    HttpClient Config Add

---
+++ src/main/java/com/springboot/jpa/config/setting/HttpClientConfig.java
@@ -0,0 +1,46 @@
+package com.springboot.jpa.config.setting;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpClientConfig {
+    
+	@Bean
+	public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+		
+		// Configure SSL – Accept All (HttpClient 4.4 and Above) 모두 허용!
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+				
+		SSLContext sslContext = SSLContexts.custom()
+									.loadTrustMaterial(null, acceptingTrustStrategy)
+									.build();
+ 
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+ 
+		CloseableHttpClient httpClient = HttpClients.custom()
+											.setSSLSocketFactory(csf)
+											.build();
+ 
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+ 
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		
+		return restTemplate;
+	}
+}
